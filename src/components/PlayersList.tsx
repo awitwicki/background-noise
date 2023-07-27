c4a936132deab176cfce1ec74cc70f20ea39f086
Andrew Witwicki <vitvitskiy3@gmail.com>
2023-07-27 22:16:03 +0200

    Fix bug with sounds urls after build

--- src/components/PlayersList.tsx
+++ src/components/PlayersList.tsx
@@ -11,58 +11,69 @@ import {
 } from 'react-icons/fa'
 import SectionPlayer from 'components/SectionPlayer'
 
+import soundBirds from '/sounds/birds.ogg'
+import soundCity from '/sounds/city.ogg'
+import soundCoffee from '/sounds/coffee-shop.ogg'
+import soundFireplace from '/sounds/fireplace.ogg'
+import soundNight from '/sounds/summer-night.ogg'
+import soundPinkNoice from '/sounds/pink-noise.ogg'
+import soundRain from '/sounds/rain.ogg'
+import soundStorm from '/sounds/storm.ogg'
+import soundWaves from '/sounds/waves.ogg'
+import soundWhiteNoice from '/sounds/white-noise.ogg'
+
 export default function () {
   return (
     <div class="flex flex-col gap-5">
       <SectionPlayer
         playerName="Rain"
         icon={<FaCloudRain />}
-        soundPath="/sounds/rain.ogg"
+        soundPath={soundRain}
       />
       <SectionPlayer
         playerName="Storm"
         icon={<FaBolt />}
-        soundPath="/sounds/storm.ogg"
+        soundPath={soundStorm}
       />
       <SectionPlayer
         playerName="Night"
         icon={<FaMoon />}
-        soundPath="/sounds/summer-night.ogg"
+        soundPath={soundNight}
       />
       <SectionPlayer
         playerName="Fire"
         icon={<FaFire />}
-        soundPath="/sounds/fireplace.ogg"
+        soundPath={soundFireplace}
       />
       <SectionPlayer
         playerName="City"
         icon={<FaCity />}
-        soundPath="/sounds/city.ogg"
+        soundPath={soundCity}
       />
       <SectionPlayer
         playerName="Birds"
         icon={<FaCrow />}
-        soundPath="/sounds/birds.ogg"
+        soundPath={soundBirds}
       />
       <SectionPlayer
         playerName="Ocean"
         icon={<FaWater />}
-        soundPath="/sounds/waves.ogg"
+        soundPath={soundWaves}
       />
       <SectionPlayer
         playerName="Coffee shop"
         icon={<FaCoffee />}
-        soundPath="/sounds/coffee-shop.ogg"
+        soundPath={soundCoffee}
       />
       <SectionPlayer
         playerName="White noise"
         icon={<FaWaveSquare />}
-        soundPath="/sounds/white-noise.ogg"
+        soundPath={soundWhiteNoice}
       />
       <SectionPlayer
         playerName="Pink noise"
         icon={<FaWaveSquare />}
-        soundPath="/sounds/pink-noise.ogg"
+        soundPath={soundPinkNoice}
       />
     </div>
   )
