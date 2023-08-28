cace28e6e55b3f9d78aee977e73f318661184e23
Andrew Witwicki <vitvitskiy3@gmail.com>
2023-08-28 22:37:19 +0200

    Add GH link

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
 import { Suspense } from 'preact/compat'
+import { FaGithub } from "react-icons/fa";
 import PlayersList from 'components/PlayersList'
 
 export default function () {
   return (
     <div className="container mx-auto max-w-prose p-10 prose">
-      <h1 class="font-normal">Chill</h1>
-      <Suspense fallback={<p>Loading...</p>}>
-        <PlayersList />
-      </Suspense>
+        <div className="flex items-center mb-8">
+            <h1 className="font-normal mr-2 mb-0">Chill</h1>
+            <a
+                href="https://github.com/awitwicki/background-noise"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                <FaGithub size={30} />
+            </a>
+        </div>
+        <Suspense fallback={<p>Loading...</p>}>
+          <PlayersList />
+        </Suspense>
     </div>
   )
 }
